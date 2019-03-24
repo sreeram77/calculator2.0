56d565dc6c62a5ce6d9cdff91a49bf1f4c57ffad
Sreeram Narayanan <sreeram.an1994@gmail.com>
2019-03-24 21:12:15 +0530

    Implements Value updating in Calculator display

---
+++ app/src/main/java/com/example/myapplication/Calc.java
@@ -0,0 +1,18 @@
+package com.example.myapplication;
+
+public class Calc {
+
+    private double dispNum;
+
+    public Calc() {
+        dispNum = 0;;
+    }
+
+    public double getDispNum() {
+        return dispNum;
+    }
+
+    public void setDispNum(double dispNum) {
+        this.dispNum = dispNum;
+    }
+}
--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.myapplication;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
@@ -10,6 +11,7 @@ public class MainActivity extends AppCompatActivity {
     TextView calcDisplay;
     Button btnNum1, btnNum2, btnNum3, btnNum4, btnNum5, btnNum6, btnNum7, btnNum8, btnNum9, btnNum0;
     Button btnAdd, btnEquals, btnMul, btnSub, btnDiv;
+    Calc calc = new Calc();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -33,5 +35,84 @@ public class MainActivity extends AppCompatActivity {
         btnMul = findViewById(R.id.buttonMul);
         btnEquals = findViewById(R.id.buttonEqual);
 
+        btnNum1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                calcDisplay.setText(Double.toString(getDisplayNumber(1)));
+            }
+        });
+
+        btnNum2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                calcDisplay.setText(Double.toString(getDisplayNumber(2)));
+            }
+        });
+
+        btnNum3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                calcDisplay.setText(Double.toString(getDisplayNumber(3)));
+            }
+        });
+
+        btnNum4.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                calcDisplay.setText(Double.toString(getDisplayNumber(4)));
+            }
+        });
+
+        btnNum5.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                calcDisplay.setText(Double.toString(getDisplayNumber(5)));
+            }
+        });
+
+        btnNum6.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                calcDisplay.setText(Double.toString(getDisplayNumber(6)));
+            }
+        });
+
+        btnNum7.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                calcDisplay.setText(Double.toString(getDisplayNumber(7)));
+            }
+        });
+
+        btnNum8.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                calcDisplay.setText(Double.toString(getDisplayNumber(8)));
+            }
+        });
+
+        btnNum9.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                calcDisplay.setText(Double.toString(getDisplayNumber(9)));
+            }
+        });
+
+        btnNum0.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                calcDisplay.setText(Double.toString(getDisplayNumber(0)));
+            }
+        });
+
+    }
+
+    public double getDisplayNumber(double num){
+
+        if (calc.getDispNum() != 0) {
+            num = (calc.getDispNum() * 10) + num;
+        }
+        calc.setDispNum(num);
+        return num;
     }
 }
